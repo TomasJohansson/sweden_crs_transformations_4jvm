66fadfb983e727b26a41c8fc4ed83cbd0a01f1f2
Tomas Johansson <github-tomas@programmerare.com>
2021-03-16 21:47:19 +0100

    Copied two test files from the corresponding TypeScript project 'sweden_crs_transformations_4typescript' (since the C#.NET project 'sweden_crs_transformations_4net' otherwise copied from does currently not have those similar test files) https://github.com/TomasJohansson/sweden_crs_transformations_4typescript/tree/typescript_SwedenCrsTransformations/sweden_crs_transformations_4typescript/test/mighty_little_geodesy The latest (as of 2021-03-16) versions of those files were copied (i.e. as of git commit '2bfce537' at date 2021-02-17)

---
+++ lib/sweden_crs_transformations_4typescript/lat_lon.test.ts
@@ -0,0 +1,7 @@
+﻿import LatLon from '../../src/mighty_little_geodesy/lat_lon';
+
+test('LatLon', () => {
+  const latLon = new LatLon(12.34, 56.78);
+  expect(latLon.yLatitude).toEqual(12.34);
+  expect(latLon.xLongitude).toEqual(56.78);
+});
