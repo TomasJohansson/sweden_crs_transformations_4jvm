82366d7e3f536463b753132cbf85157c34e9a329
Tomas Johansson <github-tomas@programmerare.com>
2021-03-18 15:05:04 +0100

    Comments at the top of the files i.e. the license section.

--- lib/src/main/java/com/programmerare/sweden_crs_transformations_4jvm/CrsProjection.java
+++ lib/src/main/java/com/programmerare/sweden_crs_transformations_4jvm/CrsProjection.java
@@ -1,12 +1,13 @@
 /*
-* Copyright (c) Tomas Johansson , http://www.programmerare.com
-* The code in this library is licensed with MIT.
-* The library is based on the library 'MightyLittleGeodesy' (https://github.com/bjornsallarp/MightyLittleGeodesy/) 
-* which is also released with MIT.
-* License information about 'sweden_crs_transformations_4net' and 'MightyLittleGeodesy':
-* https://github.com/TomasJohansson/sweden_crs_transformations_4net/blob/csharpe_SwedenCrsTransformations/LICENSE
-* For more information see the webpage below.
-* https://github.com/TomasJohansson/sweden_crs_transformations_4net
+ * Copyright (c) Tomas Johansson , http://www.programmerare.com
+ * The code in this library is licensed with MIT.
+ * The library is based on the C#.NET library 'sweden_crs_transformations_4net' (https://github.com/TomasJohansson/sweden_crs_transformations_4net)
+ * which in turn is based on 'MightyLittleGeodesy' (https://github.com/bjornsallarp/MightyLittleGeodesy/)
+ * which is also released with MIT.
+ * License information about 'sweden_crs_transformations_4jvm' and 'MightyLittleGeodesy':
+ * https://github.com/TomasJohansson/sweden_crs_transformations_4jvm/blob/java_SwedenCrsTransformations/LICENSE
+ * For more information see the webpage below.
+ * https://github.com/TomasJohansson/sweden_crs_transformations_4jvm
 */
 package com.programmerare.sweden_crs_transformations_4jvm;
 
