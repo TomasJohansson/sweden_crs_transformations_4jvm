bd9118227c0ab612395ebd49024f590837c193f5
Tomas Johansson <github-tomas@programmerare.com>
2021-03-20 14:25:54 +0100

    Deleted obsolete/deprecated files

--- lib/src/test/java/com/programmerare/sweden_crs_transformations_4jvm/JavaCrsProjectionFactoryTest.java
+++
@@ -1,5 +0,0 @@
-package com.programmerare.sweden_crs_transformations_4jvm;
-
-public class JavaCrsProjectionFactoryTest
-{
-}
--- lib/src/test/kotlin/com/programmerare/sweden_crs_transformations_4jvm/KotlinCrsProjectionFactoryTest.kt
+++
@@ -1,4 +0,0 @@
-package com.programmerare.sweden_crs_transformations_4jvm
-
-class KotlinCrsProjectionFactoryTest {
-}
